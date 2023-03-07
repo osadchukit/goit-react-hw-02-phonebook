405ad0e790168282899248cb848a8c2a1632f170
Ivan Osadchuk <osadchukit@gmail.com>
2023-03-07 22:18:22 +0100

    update 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,18 +7,27 @@ import { Box } from './App.styled';
 class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
   addContact = newContact => {
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, newContact] };
-    });
+    const isExist = this.state.contacts.find(
+      contact =>
+        contact.name === newContact.name || contact.number === newContact.number
+    );
+    if (isExist) {
+      alert(`${newContact.name}: is already in contacts`);
+      return;
+    } else {
+      this.setState(prevState => {
+        return { contacts: [...prevState.contacts, newContact] };
+      });
+    }
   };
 
   deleteContact = contactId => {
